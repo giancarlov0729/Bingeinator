6093c20ab9846086d2475c68d9a4329d20e7037a
MiguelGoizueta <goizuetamiguel@ufl.edu>
2024-12-02 20:37:56 -0500

    testing to see if miguel is correctly connected to the repository

---
+++ src/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by CHUMA'S on 12/2/24.
+//
+
+#include "test.h"
---
+++ src/test.h
@@ -0,0 +1,14 @@
+//
+// Created by CHUMA'S on 12/2/24.
+//
+
+#ifndef BINGEINATOR_TEST_H
+#define BINGEINATOR_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //BINGEINATOR_TEST_H
