ef4bad465a4145bf91c8d2b2911bab89f3083918
giancarlo_vidal <gvidal0729@gmail.com>
2024-12-03 23:40:32 -0500

    Modified the readCSV function to output a vector of data so later be used to build the max heap within the reccomend function. Implemented the quicksort as well as the quicksort reccomend function

--- src/main.cpp
+++ src/main.cpp
@@ -234,10 +234,10 @@ int main() {
     bool whichAlgo = true;
     if (input == "m" || input == "M") {
         cout << "You chose Max Heap.\n";
-        readCSV("tvs.csv", data);
+        readCSV("src/tvs.csv", data);
     } else if (input == "q" || input == "Q") {
         cout << "You chose Quicksort.\n";
-        readCSV("tvs.csv", data);
+        readCSV("src/tvs.csv", data);
         whichAlgo = false;
     } else {
         cout << "Invalid input.\n";
